d22e64d4390178f11098c158086d9c02efa0a47a
Larissa Gärtner <larissa3001@hotmail.de>
2015-12-21 01:00:35 +0100

    Mehrere Objekte
    mehrere, verschiedene Objekte erstellt - neue Bewegung und neue
    SpawnMethode

--- Music Game - Bachelor/Assets/Scripts/ObjectMovement.cs
+++ Music Game - Bachelor/Assets/Scripts/ObjectMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ObjectMovement : MonoBehaviour
 {
+    public GameObject obj;
 
     // Use this for initialization
     void Start()
@@ -13,6 +14,9 @@ public class ObjectMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       Vector3 vec = obj.transform.position;
+
+        obj.transform.position = new Vector3(vec.x - 0f, vec.y - 0.025f, vec.z - 0f);
 
     }
 
---
+++ Music Game - Bachelor/Assets/Scripts/SpawnObject1.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObject1 : MonoBehaviour
+{
+
+    public GameObject Snow;
+
+    public float x;
+    public float y;
+    public float z;
+
+
+    float spawnCooldown = 4;
+    float timeUntilSpawn = 4;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnObjects();
+        // Snow.transform.position = new Vector3(sVec.x,sVec.y,sVec.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timeUntilSpawn -= Time.deltaTime;
+        if (timeUntilSpawn <= 0)
+        {
+            SpawnObjects();
+
+            timeUntilSpawn = spawnCooldown;
+        }
+
+    }
+
+    void SpawnObjects()
+    {
+
+        Vector3 startPos1 = new Vector3(x, y, z);
+
+        GameObject spawnOb = Instantiate(Snow, startPos1, Quaternion.identity) as GameObject;
+
+    }
+}
