0782de103b8a6f34f32e19df88bb036820e9c854
Larissa Gärtner <larissa3001@hotmail.de>
2016-01-06 01:46:51 +0100

    Musik in der richtigen Ordnung eingefügt / Levelwechsel / Anwendung zur Probe

---
+++ Music Game - Bachelor/Assets/Scripts/Level1Button.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Application.loadedLevelName == "Scene2")
+
+                GlobalValues.globalClicksObj1 = 0;
+                GlobalValues.globalClicksObj2 = 0;
+                GlobalValues.globalClicksObj3 = 0;
+                GlobalValues.globalClicksObj4 = 0;
+                GlobalValues.globalClicksObj5 = 0;
+
+            Application.LoadLevel("Scene1");
+
+            
+        }
+
+    }
+}
---
+++ Music Game - Bachelor/Assets/Scripts/Level2Button.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2Button : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseOver(){
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Application.loadedLevelName == "Scene1")
+            
+                GlobalValues.globalClicksObj1 = 0;
+                GlobalValues.globalClicksObj2 = 0;
+                GlobalValues.globalClicksObj3 = 0;
+                GlobalValues.globalClicksObj4 = 0;
+                GlobalValues.globalClicksObj5 = 0;
+
+            Application.LoadLevel("Scene2");
+
+        }
+
+    }
+}
